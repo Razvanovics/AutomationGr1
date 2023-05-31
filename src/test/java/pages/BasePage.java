05138c8015b57c620db6461bbf300fe61b34b353
Razvanovics <razvan.donosa@gmail.com>
2023-05-31 21:40:34 +0300

    Initial commit

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,11 @@
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasePage {
+    private static final Logger LOG = LoggerFactory.getLogger(BasePage.class);
+    public static WebDriver driver;
+}
